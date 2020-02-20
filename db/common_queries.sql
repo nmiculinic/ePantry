f5f5e272580c17b3bcbf96aabe7b3dab69c88f39
Neven Miculinic <neven.miculinic@gmail.com>
2020-02-20 19:05:21 +0100

    Added new queries to common_queries.sql

--- db/common_queries.sql
+++ db/common_queries.sql
@@ -1,4 +1,16 @@
--- display food diary
+-- display abbrev food diary
+
+SELECT food_diary.time,
+       food_recipes.name,
+       food_recipes.meal_type,
+       food_diary.notes
+FROM food_diary,
+     food_recipes
+WHERE food_diary.recipe_id = food_recipes.id
+  AND food_diary.time > ?
+ORDER BY food_diary.time
+
+-- display food diary with details
 
 SELECT food_diary.time,
        food_recipes.name,
@@ -13,3 +25,37 @@ WHERE food_diary.recipe_id = food_recipes.id
   AND food_recipes.id = food_recipe_item.recipe_id
   AND items.id = food_recipe_item.item_id
 ORDER BY food_diary.time
+
+
+-- meal prep meals
+
+SELECT food_recipes.name, food_recipes.meal_type, amounts.amount
+FROM (
+         SELECT food_diary.recipe_id, COUNT(*) as "amount"
+         FROM food_diary
+         WHERE food_diary.time > ?
+         GROUP BY food_diary.recipe_id
+     ) AS amounts,
+     food_recipes
+WHERE food_recipes.id = amounts.recipe_id
+
+-- shopping list
+
+
+SELECT
+    -- food_recipes.id,
+    items.id,
+    items.name,
+    -- food_recipe_item.amount_kilogram
+    SUM(food_recipe_item.amount_kilogram) * 1000.0 AS "g",
+    COUNT(*)                                       AS "times repeated"
+FROM food_diary,
+     food_recipes,
+     food_recipe_item,
+     items
+WHERE food_diary.recipe_id = food_recipes.id
+  AND food_recipes.id = food_recipe_item.recipe_id
+  AND items.id = food_recipe_item.item_id
+  AND food_diary.time > '2020-02-21'
+GROUP BY items.id
+ORDER BY items.name
