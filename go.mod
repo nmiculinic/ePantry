46e2c3c70dd27828e9dbfe80a7152359bb2b2e07
Neven Miculinic <neven.miculinic@gmail.com>
2020-03-08 21:30:39 +0100

    Initial todoist integration

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-kit/kit v0.8.0
 	github.com/lib/pq v1.3.0
 	github.com/prometheus/common v0.4.0
+	github.com/sachaos/todoist v0.15.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.4.0
--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -5,12 +5,16 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/nmiculinic/ePantry/pkg/cli/generate"
+	"github.com/nmiculinic/ePantry/pkg/cli/todoist"
 )
 
 func NewLocalServerCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "ePantry",
 	}
-	cmd.AddCommand(generate.NewGenerateCommand())
+	cmd.AddCommand(
+		generate.NewGenerateCommand(),
+		todoist.NewCommand(),
+	)
 	return cmd
 }
